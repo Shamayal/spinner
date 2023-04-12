83814ff1cd4bd9fb8c724d2cfadc694ce6dc1004
Shamayal Syed <shamayalsyed9@gmail.com>
2023-04-12 01:41:15 -0400

    loading spinner refactored

---
+++ spinner2.js
@@ -0,0 +1,9 @@
+let spinner = ["|", "/", "-", "\\", "|", "/", "-", "\\", "|"];
+
+let delay = 100;
+for (const spin of spinner) {
+  setTimeout(() => {
+    process.stdout.write(`\r${spin}   `);
+  }, delay);
+  delay += 200;
+};
